3de4875ed01c8d607a501293299517d5f66ebddd
FacundoMajda <facundomajda13@gmail.com>
2023-08-04 06:37:57 -0300

    wea

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { Server as SocketServer } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer();
+const io = new SocketServer(server);
 
-app.listen(3000);
+server.listen(3000);
 console.log("listening on port 3000");
